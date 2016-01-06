979fefd0a4a5bca06c0c5bc48c761cb1488cb600
Joseph Pachev <josephpachev@gmail.com>
2016-01-05 20:35:57 -0700

    Added hello, loop, and prime.

---
+++ hello.c
@@ -0,0 +1,13 @@
+  /* Hello World program */
+
+  #include<stdio.h>
+
+   int main()
+{
+  long long int a = 38194;
+  printf("Hello World\n");
+  printf ("Decimals: %d foooooolish %ld %d\n", 1977, 650000L, a);
+ printf("Storage size for int : %lu \n", sizeof(a));
+  return 0; 
+}
+
---
+++ prime.c
@@ -0,0 +1,19 @@
+ #include<stdio.h>
+ #include <stdlib.h>
+ /* function returning the max between two numbers */
+int prime(int n) 
+{
+  return 0; 
+}
+
+
+int main(int argc, char** argv)
+{
+  if (argc < 2)
+  {
+    fprintf(stderr, "Missing argument\n");
+    exit(1);
+  }
+  
+  return 0; 
+}

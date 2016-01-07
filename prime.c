9d5fd489fad03ee999f86f65cc688dde9df8a6db
Joseph Pachev <josephpachev@gmail.com>
2016-01-06 20:01:16 -0700

    Work in progress on prime tester.

--- prime.c
+++ prime.c
@@ -1,19 +1,23 @@
  #include<stdio.h>
  #include <stdlib.h>
- /* function returning the max between two numbers */
+ /* function calculating primes*/
 int prime(int n) 
 {
+  printf("prime called with n = %d\n",n);
   return 0; 
 }
 
-
 int main(int argc, char** argv)
 {
+  int arg_n;
+  
   if (argc < 2)
   {
     fprintf(stderr, "Missing argument\n");
     exit(1);
   }
   
+  arg_n = atoi(argv[1]);
+  prime(arg_n); 
   return 0; 
 }

8697ad76bc5d7d7175fdba82b1a77a0cbf296c59
Joseph Pachev <josephpachev@gmail.com>
2016-01-25 20:43:06 -0700

    Prime factorizer is done! Yaw hoo!

--- primefact.c
+++ primefact.c
@@ -6,32 +6,36 @@
 void primefact (long long int n) 
 {
   long long int scan_n = sqrt(n);
-  int i;
+  long long int i;
   int a = 0;
-  printf ("primefact called with n = %lld scan_n = %lld\n", n, scan_n);
-  
-  if (n == 2)
-    return;
   
   while (n % 2 == 0)
   {
     n = n/2;
     a++;
   }
-  printf("2^%d\n",a);
-  if (n % 2 == 0)
-  {
-    
-  }
+  
+  if(a)
+    printf("2^%d\n",a); 
  
   for (i = 3; i <= scan_n; i += 2)
   {
-    if (n % i == 0)
+    a = 0;
+    
+    while (n % i == 0)
     {
-      printf ("%d\n",i);
-      return
+      a ++;
+      n /= i;
+    }
+    
+    if (a)
+    {
+      printf("%lld^%d\n",i,a);
     }
   }
+  
+  if (n != 1)
+   printf("%lld^1\n", n);
   
   return; 
 }
@@ -49,8 +53,4 @@ int main(int argc, char** argv)
  arg_n = atoll(argv [1] );
  primefact (arg_n);
  return 0;
-} 
-  
- 
-   
-                
+}                

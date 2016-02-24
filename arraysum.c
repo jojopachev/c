9da2c402984e8f62a18f2963ed727b66f11642cd
Joseph Pachev <josephpachev@gmail.com>
2016-02-24 12:18:01 -0700

    Solved array sum Hackerrank challenge.

---
+++ arraysum.c
@@ -0,0 +1,26 @@
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+
+int main()
+{
+    int n; 
+    scanf("%d",&n);
+    int arr[n];
+    int arr_i;
+    int s = 0;
+    
+    for( arr_i = 0; arr_i < n; arr_i++)
+    {     
+       scanf("%d",&arr[arr_i]);
+       s += arr[arr_i];
+    } 
+    
+    printf("%d\n", s);
+    return 0;
+}
+
